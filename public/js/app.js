67d9f756dbaffdce82b2c254ba9dc2e647028f74
bettoraite <dnbspro@gmail.com>
2023-09-16 13:31:24 +0500

    sidebar replay button

--- public/js/app.js
+++ public/js/app.js
@@ -3,23 +3,25 @@ const elementFormulaContainer = document.getElementById("element-formula-contain
 const quizInput = document.getElementById("quiz-input");
 const skipButton = document.getElementById("quiz-skip-btn");
 const replayButton = document.getElementById("quiz-replay-btn");
+const sidebarReplayButton = document.getElementById("sidebar-replay-btn");
 const submitAnswerButton = document.getElementById("quiz-submit-answer-btn");
 const quizTitle = document.getElementById("quiz-title");
 const optionsMenuToggleButtons = document.querySelectorAll(".quiz__options-menu-toggle-btn");
 const quizOptionsMenu = document.getElementById("quiz-options-menu");
 const randomModeToggle = document.getElementById("quiz-random-mode-checkbox");
-const swapFormulaName = document.getElementById("quiz-swap-formula-name-btn");
+const swapFormulaNameButton = document.getElementById("quiz-swap-formula-name-btn");
+
 const alkanes = {
   CH4: "Метан",
   C2H6: "Этан",
-  C3H8: "Пропан",
-  C4H10: "Бутан",
-  C5H12: "Пентан",
-  C6H14: "Гексан",
-  C7H16: "Гептан",
-  C8H18: "Октан",
-  C9H20: "Нонан",
-  C1H22: "Декан",
+  // C3H8: "Пропан",
+  // C4H10: "Бутан",
+  // C5H12: "Пентан",
+  // C6H14: "Гексан",
+  // C7H16: "Гептан",
+  // C8H18: "Октан",
+  // C9H20: "Нонан",
+  // C1H22: "Декан",
 };
 const radicals = {
   CH3: "Метил",
@@ -121,11 +123,16 @@ skipButton.addEventListener("click", () => {
 });
 replayButton.addEventListener("click", () => {
   init();
+  replayButton.classList.add("quiz__replay-btn--hidden");
 });
-swapFormulaName.addEventListener("click", () => {
+sidebarReplayButton.addEventListener("click", () => {
+  init();
+  sidebarReplayButton.classList.toggle("quiz__sidebar-btn--active");
+});
+swapFormulaNameButton.addEventListener("click", () => {
   elementsList.swapFormulaName();
   init();
-  swapFormulaName.classList.toggle("quiz__swap-formula-name-btn--active");
+  swapFormulaNameButton.classList.toggle("quiz__sidebar-btn--active");
 });
 optionsMenuToggleButtons.forEach((menuToggleButton) => {
   menuToggleButton.addEventListener("click", () => {
@@ -148,6 +155,7 @@ function moveToNextQuestion() {
   displayElementName();
   if (elementsList.isListFinished()) {
     quizInput.disabled = true;
+    replayButton.classList.remove("quiz__replay-btn--hidden");
   } else {
     hideElementFormula();
   }
@@ -165,7 +173,7 @@ function displayElementName() {
 function displayElementFormula() {
   const elementFormula = elementsList.getCurrentElementFormula();
   elementFormulaContainer.innerText = elementFormula ?? "nope";
-  elementFormulaContainer.classList.remove("quiz__element-formula-container--hidden");
+  elementFormulaContainer.classList.toggle("quiz__element-formula-container--hidden");
 }
 function hideElementFormula() {
   elementFormulaContainer.classList.add("quiz__element-formula-container--hidden");
